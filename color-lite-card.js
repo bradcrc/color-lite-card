5a417edca291ac45a825a34a883ddc4e5294ba76
bradcrc <42559592+bradcrc@users.noreply.github.com>
2019-11-27 23:53:45 -0600

    Update color-lite-card.js

--- color-lite-card.js
+++ color-lite-card.js
@@ -3,35 +3,25 @@ class ColorLite extends HTMLElement {
     if (!this.content) {
       const card = document.createElement('ha-card');  
       this.content = document.createElement('div');	
-      card.appendChild(this.content);
-	  
-	  
-      this.appendChild(card);
-	  
+      card.appendChild(this.content);	  
+	  card.style.background = 'none';	  
+      this.appendChild(card);	  
     }
 
     const entityId = this.config.entity;  	
 	const state = hass.states[entityId];
-	
-	
+		
 	
 //  if the light is on	
 
-
 	if(state.state == 'on'){
-
 	
 		const imageURLId = this.config.image;			
-		var ImURL = imageURLId;
-		
-		const imageURLCId = this.config.color_image;			
-  
-	
+		var ImURL = imageURLId;		
+		const imageURLCId = this.config.color_image;				
 		var rgbval = state.attributes.rgb_color;			
-		var hsval = state.attributes.hs_color;	
-		
-		var hsar = "";	
-		
+		var hsval = state.attributes.hs_color;			
+		var hsar = "";			
 		if (hsval) {
 			if (rgbval != "255,255,255") {				
 				var hsar = ' hue-rotate(' + hsval[0] + 'deg)';			
@@ -39,43 +29,28 @@ class ColorLite extends HTMLElement {
 				ImURL = imageURLCId;		
 				}
 			}
-		}
-	
-		
+		}		
 		var bbritef = state.attributes.brightness;	
-		var bbrite = (bbritef / 255);
-	
-	
-	
+		var bbrite = (bbritef / 255);	
 	
 		this.content.innerHTML = `	
 <!-- Custom Lite Card for x${rgbval}x -->	
 <img src="${ImURL}" style="filter: opacity(${bbrite})${hsar}!important;" width="100%" height="100%">
 `;
 	
-	} else {
-			
+	} else {			
 		this.content.innerHTML = `
 	<!-- Custom Lite Card for ${entityId} is turned off -->
-	`;	
-		
-	}
-	
-	
-  }
-
-  
-  
-  
+	`;			
+	}	
+  }  
   
   setConfig(config) {
     if (!config.entity) {
       throw new Error('You need to define an entity');
     }
     this.config = config;
-  }
-
-  
+  }  
   
   // The height of your card. Home Assistant uses this to automatically
   // distribute all cards over the available columns.
