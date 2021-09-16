c14dd0c9984e603ca470a45d2bc594a363bc4f14
bradcrc <42559592+bradcrc@users.noreply.github.com>
2021-09-16 18:34:57 -0500

    Update color-lite-card.js

--- color-lite-card.js
+++ color-lite-card.js
@@ -1,55 +1,63 @@
+// COLOR LITE CARD
+
+// Simple custom card for displaying lights on a picture elements floor plan
+
+
 class ColorLite extends HTMLElement {
   set hass(hass) {
     if (!this.content) {
-      const card = document.createElement('ha-card');  
-      this.content = document.createElement('div');	
-      card.appendChild(this.content);	  
-	  card.style.background = 'none';	  
-      this.appendChild(card);	  
+      const card = document.createElement('ha-card');
+      this.content = document.createElement('div');
+      card.appendChild(this.content);
+	  card.style.background = 'none';
+      this.appendChild(card);
     }
 
-    const entityId = this.config.entity;  	
+    const entityId = this.config.entity;
 	const state = hass.states[entityId];
-		
-	
-//  if the light is on	
+
+ 
+//  if the light is on
 if(state){
 	if(state.state == 'on'){
-	
-		const imageURLId = this.config.image;			
-		var ImURL = imageURLId;		
-		const imageURLCId = this.config.color_image;				
-		var rgbval = state.attributes.rgb_color;			
-		var hsval = state.attributes.hs_color;			
+
+		const imageURLId = this.config.image;
+		var ImURL = imageURLId;
+		const imageURLCId = this.config.color_image;
+		var rgbval = state.attributes.rgb_color;
+		var hsval = state.attributes.hs_color;
+		var ctemp = state.attributes.color_temp;
 		var hsar = "";
 		var min_bright = (this.config.min_brightness * 2.5);
 		var bright = state.attributes.brightness;
+
 		if (hsval) {
-			if (rgbval != "255,255,255") {				
-				var hsar = ' hue-rotate(' + hsval[0] + 'deg)';			
+			if (ctemp || rgbval == "255,255,255") {
+			} else {
+				var hsar = ' hue-rotate(' + hsval[0] + 'deg)';
 				if (imageURLCId) {
-				ImURL = imageURLCId;		
+				ImURL = imageURLCId;
 				}
 			}
-		}		
-		var bbritef = bright;	
+		}
+		var bbritef = bright;
 		if (min_bright > bright) {
 			bbritef = min_bright;
 		}
 		var bbrite = (bbritef / 205);
 	
-		this.content.innerHTML = `	
-<!-- Custom Lite Card for x${rgbval}x -->	
+		this.content.innerHTML = `
+<!-- Custom Lite Card 6  -->	
 <img src="${ImURL}" style="filter: opacity(${bbrite})${hsar}!important;" width="100%" height="100%">
 `;
-	
-	} else {			
+
+	} else {
 		this.content.innerHTML = `
 	<!-- Custom Lite Card for ${entityId} is turned off -->
-	`;			
-	}	
-  }  
-}  
+	`;
+	}
+  }
+}
 
 
   setConfig(config) {
@@ -57,10 +65,9 @@ if(state){
       throw new Error('You need to define an entity');
     }
     this.config = config;
-  }  
-  
-  // The height of your card. Home Assistant uses this to automatically
-  // distribute all cards over the available columns.
+  }
+
+
   getCardSize() {
     return 3;
   }
