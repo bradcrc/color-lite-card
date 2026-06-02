3b3051a87263cf5f22ab69d2bbc46da4c6f289c6
bradcrc <42559592+bradcrc@users.noreply.github.com>
2026-06-01 21:18:00 -0500

    Update color-lite-card.js

--- color-lite-card.js
+++ color-lite-card.js
@@ -16,11 +16,9 @@ class ColorLite extends HTMLElement {
       this.img.style.width = '100%'; this.img.style.height = '100%';
       this.img.style.display = 'block'; this.img.style.pointerEvents = 'none';
       this.img.style.transition = 'opacity 0.7s ease-in-out, filter 0.7s ease-in-out';
-      
       this.content.appendChild(this.img);
       card.appendChild(this.content);
       this.appendChild(card);
-      
       this._prevState = {};
       this._lastColor = { hs: [0, 0], rgb: [255, 255, 255] };
     }
@@ -34,42 +32,24 @@ class ColorLite extends HTMLElement {
       const hs = this._lastColor.hs;
       const rgb = this._lastColor.rgb;
       const bright = state.attributes.brightness || 0;
-      
-      
       const targetOpacity = (state.state === 'on') ? (bright / 255) * 0.7 : 0;
-      //const isColor = (hs[1] > 5);
-      
       const CTKelvin = state.attributes.color_temp_kelvin;
       const RGBColor = state.attributes.rgb_color;
-      
       const targetIMG = this.config.image;
       const targetIMG_Color = this.config.color_image;
-	  
-	    var targetURL = targetIMG;
+	  var targetURL = targetIMG;
 
-      let filterStr = "";
- 
-//	  const ColorMode = state.attributes.color_mode;	
-//	  var isColor = false;
-//	  if (ColorMode == "rgb" || ColorMode == "hs" || ColorMode == "xy") {
-//		  isColor = true;		  
-//	  }
-
-	  
+      let filterStr = "";  
       if (CTKelvin || !RGBColor ) {
-
         filterStr = `brightness(1.2)`;	
-          
       } else {
           if (targetIMG_Color) {
             targetURL = targetIMG_Color;
             filterStr = ` hue-rotate(${hs[0] - 0}deg)`;
-            //var hsar = ' hue-rotate(' + hsval[0] + 'deg)';
           } else {		  
             filterStr = `brightness(.4) sepia(4) saturate(1800%) hue-rotate(${hs[0] - 20}deg)`;
           }
       }
-      
       
       filterStr += ` drop-shadow(0 0 5px rgba(${rgb[0]},${rgb[1]},${rgb[2]},0.2))`;
 
